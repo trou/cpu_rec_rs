0c9fac2a064ec735253c3abf4904f949160d5d92
Raphaël Rigo <devel-git@syscall.eu>
2023-06-11 17:37:35 +0200

    poc working

--- src/corpus.rs
+++ src/corpus.rs
@@ -5,11 +5,10 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-static BASE_COUNT: f64 = 0.01;
 
 #[derive(Debug)]
 pub struct CorpusStats {
-    arch: String,
+    pub arch: String,
     bigrams_freq: HashMap<(u8, u8), f64>,
     trigrams_freq: HashMap<(u8, u8, u8), f64>,
     bg_base_freq: f64,
@@ -17,24 +16,27 @@ pub struct CorpusStats {
 }
 
 impl CorpusStats {
-    pub fn new(arch: String, file: &Path) -> Self {
+    pub fn new(arch: String, file: &Path, base_count: f64) -> Self {
         let mut bg: HashMap<(u8, u8), f64> = HashMap::new();
         let mut tg = HashMap::new();
         let mut buf = Vec::new();
         debug!("Loading {} for arch {}", file.display(), arch);
         File::open(file).unwrap().read_to_end(&mut buf).unwrap();
 
+        /*
+        Duplicate code to be able to use tuples, for optimization
+        */
         for w in buf.windows(2) {
             let b = (w[0], w[1]);
             bg.entry(b)
                 .and_modify(|count| *count += 1.0)
-                .or_insert(1.0 + BASE_COUNT);
+                .or_insert(1.0 + base_count);
         }
         for w in buf.windows(3) {
             let b = (w[0], w[1], w[2]);
             tg.entry(b)
                 .and_modify(|count| *count += 1.0)
-                .or_insert(1.0 + BASE_COUNT);
+                .or_insert(1.0 + base_count);
         }
         debug!(
             "{}: {} bytes, {} bigrams, {} trigrams",
@@ -44,10 +46,10 @@ impl CorpusStats {
             tg.len()
         );
         let bi_qtotal: f64 =
-            (BASE_COUNT * ((u32::pow(256, 2) - bg.len() as u32) as f64)) + bg.values().sum::<f64>();
+            (base_count * ((u32::pow(256, 2) - bg.len() as u32) as f64)) + bg.values().sum::<f64>();
         debug!("{} bigrams Qtotal: {}", arch, bi_qtotal);
         let tri_qtotal: f64 =
-            (BASE_COUNT * ((u32::pow(256, 3) - tg.len() as u32) as f64)) + tg.values().sum::<f64>();
+            (base_count * ((u32::pow(256, 3) - tg.len() as u32) as f64)) + tg.values().sum::<f64>();
         debug!("{} trigrams Qtotal: {}", arch, tri_qtotal);
 
         // Update counts to frequencies
@@ -57,8 +59,23 @@ impl CorpusStats {
             arch,
             bigrams_freq: bg_freq,
             trigrams_freq: tg_freq,
-            bg_base_freq: BASE_COUNT / bi_qtotal,
-            tg_base_freq: BASE_COUNT / tri_qtotal,
+            bg_base_freq: base_count / bi_qtotal,
+            tg_base_freq: base_count / tri_qtotal,
         }
+    }
+    pub fn compute_kl(&self, q: &Self) -> (f64, f64) {
+        let mut kld_bg = 0.0;
+        for (bg, f) in &self.bigrams_freq {
+            if *f != 0.0 {
+                kld_bg += f*((f/q.bigrams_freq.get(&bg).unwrap_or(&q.bg_base_freq))).ln();
+            }
+        }
+        let mut kld_tg = 0.0;
+        for (tg, f) in &self.trigrams_freq {
+            if *f != 0.0 {
+                kld_tg += f*((f/q.trigrams_freq.get(&tg).unwrap_or(&q.tg_base_freq))).ln();
+            }
+        }
+        (kld_bg, kld_tg)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use anyhow::{Context, Result};
 use glob::glob;
 use std::str::FromStr;
 use std::string::String;
-use log::{LevelFilter, info};
+use log::{info};
 use simple_logger;
 
 fn main() -> Result<()> {
-    simple_logger::init_with_level(log::Level::Debug).unwrap();
+    simple_logger::init_with_level(log::Level::Info).unwrap();
     info!("Test");
     let corpus_entries = glob("cpu_rec_corpus/*.corpus")
         .with_context(|| "Could not find \"cpu_rec_corpus\" directory.")?
@@ -19,10 +19,18 @@ fn main() -> Result<()> {
             CorpusStats::new(
                 arch_name,
                 &p,
+                0.01
             )
         })
         .collect();
     println!("{}", corpus_stats.len());
-    //println!("{:?}", corpus_stats);
+    let target = CorpusStats::new(String::from_str("target")?, std::path::Path::new("/bin/true"), 0.0);
+    let results : Vec<(&String, (f64, f64))> = corpus_stats.iter().map(|c| (&c.arch, target.compute_kl(&c))).collect();
+    let mut sorted_m2 : Vec<(&String, f64)> = results.iter().map(|r| (r.0, r.1.0)).collect();
+    sorted_m2.sort_unstable_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    info!("Results 2-gram: {:?}", sorted_m2) ;
+    let mut sorted_m3 : Vec<(&String, f64)> = results.iter().map(|r| (r.0, r.1.1)).collect();
+    sorted_m3.sort_unstable_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    info!("Results 3-gram: {:?}", sorted_m3) ;
     Ok(())
 }
